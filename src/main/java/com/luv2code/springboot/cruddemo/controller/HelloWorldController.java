43d7f43ec4bda050fb092d048eea36737dece02f
rasidb <rasid5888@gmail.com>
2024-01-28 14:21:32 +0300

    HttpServletRequest, model kullanımı eklendi

--- src/main/java/com/luv2code/springboot/cruddemo/controller/HelloWorldController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/HelloWorldController.java
@@ -1,19 +1,33 @@
 package com.luv2code.springboot.cruddemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class HelloWorldController {
-//html formunu göstermek için yeni method oluştur
 
     @RequestMapping("/showForm")
-    public String showForm(){
+    public String showForm() {
         return "helloworld-form";
     }
 
-            //html formunu işlemek için yeni bi method oluştur
     @RequestMapping("/processForm")
-    public String processForm(){
+    public String processForm() {
         return "helloworld";
     }
+@RequestMapping("/processForm2")
+    public String letShoutDude(HttpServletRequest request, Model model) {
+        //html formundaki istekleri oku
+        String studentName = request.getParameter("studentName");
+        //hepsini büyük harfe çevir
+        //mesajı oluştur
+        String result = "yo! " + studentName.toUpperCase();
+        //mesajı modele ekle
+        model.addAttribute("message",result);
+        return "helloworld";
+
+    }
+
 }
